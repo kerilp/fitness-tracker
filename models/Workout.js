4c31a779b8333baf4200b4ea7a620e0c7ee961e8
Keri Price <keri.lynne.p@gmail.com>
2021-10-12 21:35:11 -0400

    added model and server.js

---
+++ models/Workout.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: Date,
+  exercises: [
+      {
+          type: String,
+          name: String,
+          duration: Number,
+          distance: Number,
+          weight: Number,
+          reps: Number,
+          sets: Number
+      },
+  ],
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
