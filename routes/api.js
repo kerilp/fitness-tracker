99cdebc6d8167a0bd5611e3c5c25db4ba3db4a26
Keri Price <keri.lynne.p@gmail.com>
2021-10-12 22:35:59 -0400

    added routes

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const Workout = require('../models/Workout');
+
+router.get('/workouts', (req, res) => {
+    Workout.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post('/workouts', (req, res) => {
+    Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put('/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// router.get('/')
+
+module.exports = router;
